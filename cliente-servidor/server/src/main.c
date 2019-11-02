a1dabafe634f082ec3ad92486c379e84a26bf3ec
Dinko Fernando Yoma Silva <dinko@Dinkos-MacBook-Air.local>
2019-11-02 18:20:35 -0300

    inputs listos

--- cliente-servidor/server/src/main.c
+++ cliente-servidor/server/src/main.c
@@ -17,9 +17,64 @@ char * revert(char * message){
 }
 
 int main(int argc, char *argv[]){
-  // Se define una IP y un puerto
-  char * IP = "0.0.0.0";
-  int PORT = 8080;
+    char * IP;
+  int PORT;
+
+  int L_FLAG = 0;
+  if (argc == 6) {
+    //tiene l flag
+
+    L_FLAG = 1;
+    
+    if (strcmp(argv[1], "-l") == 0) {
+      if (strcmp(argv[2], "-i") == 0) {
+        PORT = atoi(argv[5]);
+        IP = argv[3];
+
+      } else {
+        PORT = atoi(argv[3]);
+        IP = argv[5];
+      }
+
+    } else if (strcmp(argv[3], "-l") == 0) {
+      if (strcmp(argv[1], "-i") == 0) {
+        PORT = atoi(argv[5]);
+        IP = argv[2];
+
+      } else {
+        PORT = atoi(argv[2]);
+        IP = argv[5];
+      }
+
+    } else if (strcmp(argv[5], "-l") == 0) {
+      if (strcmp(argv[1], "-i") == 0) {
+        PORT = atoi(argv[4]);
+        IP = argv[2];
+
+      } else {
+        PORT = atoi(argv[2]);
+        IP = argv[4];
+      }
+
+    }
+
+
+  }
+
+  else {
+    //no tiene l flag
+    if (strcmp(argv[1], "-i") == 0) {
+      IP = argv[2];
+      PORT = atoi(argv[4]);
+    } else {
+      IP = argv[4];
+      PORT = atoi(argv[2]);
+    }
+  }
+
+  printf("port: %d\n ip: %s\n flag: %d\n", PORT, IP, L_FLAG);
+
+  
 
   // Se crea el servidor y se obtienen los sockets de ambos clientes.
   PlayersInfo * players_info = prepare_sockets_and_get_clients(IP, PORT);
