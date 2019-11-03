62547b767216c96090b386e773ea83b1f044870b
Dinko Fernando Yoma Silva <dinko@Dinkos-MacBook-Air.local>
2019-11-02 21:21:23 -0300

    print cards

--- cliente-servidor/client/src/main.c
+++ cliente-servidor/client/src/main.c
@@ -3,6 +3,63 @@
 #include "conection.h"
 #include "comunication.h"
 
+void print_cards(char * message) {
+  int current_position = 0;
+  char ** words = malloc(sizeof(char*) * 20);
+  int * words_length = malloc(sizeof(int) * 20);
+  int * words_position = malloc(sizeof(int) * 20);
+  for (int i = 0; i < 20; i++) {
+    words_length[i] = message[current_position];
+    current_position ++;
+    words[i] = malloc(sizeof(char) * words_length[i]);
+    char * word = words[i];
+    for (int j = 0; j < words_length[i]; j++) {
+      word[j] = message[current_position];
+      current_position ++;
+    }
+    words_position[i] = message[current_position];
+    current_position ++;
+  }
+  printf("       Card 1       ");
+  //son 5 espacios
+  printf("     ");
+  printf("       Card 2       \n");
+  for (int i = 0; i < 10; i++) {
+    //card1
+    for (int j = 0; j < words_position[i]; j++) {
+      printf("-");
+    }
+    char * word = words[i];
+    for (int j = 0; j < words_length[i]; j++) {
+      printf("%c", word[j]);
+    }
+    printf("     ");
+    //card2
+    for (int j = 0; j < words_position[i + 10]; j++) {
+      printf("-");
+    }
+    word = words[i + 10];
+    for (int j = 0; j < words_length[i + 10]; j++) {
+      printf("%c", word[j]);
+    }
+    printf("\n");
+    for (int j = 0; j < 20; j++) {
+      printf("-");
+    }
+    printf("     ");
+    for (int j = 0; j < 20; j++) {
+      printf("-");
+    }
+    printf("\n");
+
+  }
+
+
+
+}
+
+
+
 char * get_input(){
   char * response = malloc(20);
   int pos=0;
@@ -18,6 +75,8 @@ char * get_input(){
 
 
 int main (int argc, char *argv[]){
+
+  
   char * IP;
   int PORT;
 
@@ -78,9 +137,11 @@ int main (int argc, char *argv[]){
 
   // Se prepara el socket
   int server_socket = prepare_socket(IP, PORT);
+  char * cards_message;
 
   // Se inicializa un loop para recibir todo tipo de paquetes y tomar una acción al respecto
   while (1){
+    break;
     int msg_code = client_receive_id(server_socket);
     
     if (msg_code == 1) { //Recibimos un mensaje del servidor
@@ -94,6 +155,8 @@ int main (int argc, char *argv[]){
       
       printf("Ingrese su mensaje: ");
       char * response = get_input();
+     
+
 
       client_send_message(server_socket, option, response);
     }
@@ -112,6 +175,82 @@ int main (int argc, char *argv[]){
 
       client_send_message(server_socket, option, response);
     }
+
+    
+    if (msg_code == 2) {
+      //CONNECTION ESTABLISHED
+      printf("Se ha establecido exitosamente la conexion con el servidor\n");
+      
+    }
+    if (msg_code == 3) {
+      //ASK NICKNAME
+      printf("Ingrese su nickname:\n");
+      char * response = get_input();
+      
+    }
+    if (msg_code == 4) {
+      //RETURN NICKNAME
+
+
+    }
+    if (msg_code == 5) {
+      //OPPONENT FOUND
+      
+    }
+    if (msg_code == 6) {
+      //SEND ID'S
+
+      
+    }
+    if (msg_code == 7) {
+      //START GAME
+
+      
+    }
+    if (msg_code == 8) {
+      //SCORES
+    }
+    if (msg_code == 9) {
+      //SEND CARDS
+      cards_message = client_receive_payload(server_socket);
+      print_cards(cards_message);
+      printf("ingrese palabra: ");
+      char * response = get_input();
+  
+      client_send_message(server_socket, 10, response);
+      printf("\n");
+    
+
+    }
+    if (msg_code == 10) {
+      //SEND WORD
+    }
+    if (msg_code == 11) {
+      //RESPONSE WORD
+    }
+    if (msg_code == 12) {
+      //ROUND WINNER/LOOSER
+    }
+    if (msg_code == 13) {
+      //END GAME
+    }
+    if (msg_code == 14) {
+      //GAME WINNER/LOOSER
+    }
+    if (msg_code == 15) {
+      //ASK NEW GAME
+    }
+    if (msg_code == 16) {
+      //ANSWER NEW GAME
+    }
+    if (msg_code == 17) {
+      //DISCONNECT
+    }
+    if (msg_code == 20) {
+      //ERROR BAD PACKAGE
+    }
+   
+    
     printf("------------------\n");
   }
 
